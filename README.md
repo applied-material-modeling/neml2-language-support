cb6492350a7797676efef05b152bd3310905081d
Gary Hu <thu@anl.gov>
2025-04-12 11:23:47 -0500

    Initial commit after fork
    - Modify README.md
    - Modify src/extension.js to handle NEML2-specific logic
    - Update package.json for extension publishing purposes

--- README.md
+++ README.md
@@ -1,165 +1,22 @@
-# moose-language-support README
+# NEML2 Language Support
 
-## Features
+Language features for NEML2 input files with extension ".nm".
 
-* Autocomplete MOOSE input files
-* Syntax highlighting for MOOSE input files and `tests` specs
-* Format MOOSE input files and `tests` specs
-* Document outlines for MOOSE input files
-* Show parameter and object documentation on mouse hover
-* C++ class snippets for MOOSE development
+## Supported features
 
-### Autocomplete
+- Formatting
+- Hover over text
 
-![autocomplete](images/autocomplete1.gif)
+## Forked from idaholab/moose-language-support
 
-Autocomplete block names (even with multiple levels in one block header) and parameters.
-
-![autocomplete](images/autocomplete2.gif)
-
-Autocomplete object names such as MeshGenerators, Variables, UserObjects, Indicators, Markers, etc.
-
-### Document outline
-
-![outline](images/outline.gif)
-
-Outline of all blocks and subblocks (with their respective types). Outline respects `active` and `inactive` parameters. Autocompletion for block names in thos parameters is also supported.
-
-### Formatting
-
-![formatting](images/format_document.gif)
-
-Format input files and `tests` specs with the MOOSE HIT format tool.
-
-### Hover
-
-![formatting](images/hover.gif)
-
-Show class and parameter documentation strings when hovering with the mouse.
-
-### Syntax highlighting
-
-Add syntax highlighting and snippets to MOOSE input files in VSCode.
-
-Syntax highlighting for:
-* Valid top level blocks
-* Valid sublevel blocks
-* Special highlight for the ```type = TypeName``` lines
-* Highlight valid Function Parser functions in parameter strings
-* Highlight valid parameters for shape function family/order and element type
-* Numbers and constants (true/false)
-
-## MOOSE
-
-MOOSE is an opensource multiphysics finite element simulation framework, developed at Idaho National Laboratory.
-
-Check out the [MOOSE Framework website](https://mooseframework.inl.gov) for more information.
-
-Contributions are greatly appreciated. Please fork this repository and open a
-pull request to add snippets, make grammar tweaks, etc.
-
-## Extension Settings
-
-This extension contributes the following settings:
-
-### HIT format
-
-* `languageServerMoose.formatStyleFile`: Path to a custom HIT format style file. Leave empty for default style.
+This is a fork of [idaholab/moose-language-support](https://github.com/idaholab/moose-language-support). The extension's language server discovery logic is heavily inspired by the upstream. Features such as code snippets and action parameter autocompletion are removed since they are not relevant to NEML2 input files. Syntax highlighting is separated out as a standalone extension ([neml2-syntax-highlighting](https://github.com/applied-material-modeling/neml2-syntax-highlighting)).
 
 ## Known Issues
 
-none
+Autocompletion is under development.
 
 ## Release Notes
 
-### 1.4.2
-Do not prompt the user at all if the `MOOSE_LANGUAGE_SERVER` environment variable is set.
-
-### 1.4.1
-Enable the MOOSE language support in 'untitled' editors. Those are editors that have no saved file on disk (for example if you create a new editor pane from the command line using `code newfile.i`).
-
-### 1.4.0
-Add an option to manually pick an executable and keep a list of recently used executables.
-
-### 1.3.2
-Bugfix: the "Allow Test Objects" option was not respected and instead assumed to be always true. Fixed and defaulted to false.
-
-### 1.3.1
-Bugfix: Do not prompt for MOOSE language server when switching to terminal tabs etc.
-
-### 1.3.0
-Revert back to older logic, which searches only parent directories, but includes `*-opt`, `*-oprof`, `*-dbg`, and `*-devel` executables.
-This should be much faster. Better error handling and more robust server restarts upon executable updates.
-
-### 1.2.0
-Do not browse for executables when the `MOOSE_LANGUAGE_SERVER` environment variable is set.
-
-### 1.1.0
-Added syntax highlighting items for test specs.
-
-### 1.0.3
-Bugfix: The behavior of the custom error handler changed. Removing it for now.
-
-### 1.0.2
-Bugfix: The `vscode-languageclient` module version was bumped, but contained a breaking change.
-
-### 1.0.1
-Update release notes.
-
-### 1.0.0
-Rewrite of the plugin using the new MOOSE language server capability. Instead of a static JSON dump the plugin
-now launches a MOOSE executable with the `--language-server` option and communicates with it through LSP.
-This new approach will allow us to vastly expand autocompletion in the future.
-Make sure your MOOSE repository is up to date!
-
-### 0.8.0
-Internal beta release for the switch over to the built-in MOOSE language server.
-
-### 0.7.1
-Make server more tolerant for restrictive binary permissions
-
-### 0.7.0
-Snippets for finite volume
-
-### 0.6.2
-Update tree-sitter-hit WASM for VSCode 1.82
-### 0.6.1
-Revert WASP based formatter
-
-### 0.6.0
-Additional snippets for finite volume. Improved tests syntax.
-
-### 0.5.1
-Support for the new MooseEnum item documentation strings from https://github.com/idaholab/moose/issues/23004
-
-### 0.5.0
-Added a definition provider to jump from `type` parameter values in inout files to the class registration in the source code
-
-### 0.3.0
-Added a hover provider to show parameter and type documentation in MOOSE input files
-Enabled formatting of test specs
-Updated syntax highlighting for test specs
-
-### 0.2.0
-Added support for active/inactive parameters with completion, and responsive outline
-Added automatic indentation, commenting, autoclosing pairs (with Max Nezdyur)
-Fixed multipath completion (e.g. `[Modules/TensorMechanics/...`)
-
-### 0.1.1
-Add reduced detail outline and make it default
-
-### 0.1.0
-Document outline view for MOOSE input files
-
-### 0.0.5
-Add icon
-
-### 0.0.3
-Small fixes to the README and ignore files
-
-### 0.0.2
-Added a format provider for MOOSE input files.
-
 ### 0.0.1
 
-This extension is a port of the `language-moose`, `autocomplete-moose`, and `moose-hit-format` Atom extensions. By Daniel Schwen.
+Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -1,329 +1,244 @@
-//* This file is part of the MOOSE framework
-//* https://www.mooseframework.org
-//*
-//* Licensed under LGPL 2.1, please see LICENSE for details
-//* https://www.gnu.org/licenses/lgpl-2.1.html
+// Licensed under LGPL 2.1, please see LICENSE for details
+// https://www.gnu.org/licenses/lgpl-2.1.html
 
 import * as fs from 'fs';
 import * as process from 'process';
-import * as path from 'path';
-import { formatDistance } from 'date-fns';
-import { window, commands, workspace, ExtensionContext, Disposable, QuickPickItem, QuickPickItemKind, Uri, TextDocument, FileType } from 'vscode';
+import {
+	window,
+	commands,
+	workspace,
+	ExtensionContext,
+	QuickPickItem,
+	QuickPickItemKind,
+	Uri,
+	TextDocument
+} from 'vscode';
 
 import {
-    LanguageClient,
-    LanguageClientOptions,
-    ServerOptions,
-    TransportKind,
-    NotificationType,
-    NotificationType0,
-    State,
-    ErrorAction,
-    CloseAction
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions
 } from 'vscode-languageclient/node';
-import { count, error } from 'console';
-import { Message } from 'vscode-jsonrpc';
 
 let client: LanguageClient | null = null;
-let currentDocument: TextDocument | null = null;
-let my_context: vscode.ExtensionContext;
+let doc: TextDocument | null = null;
+let ctx: ExtensionContext;
 
-// these must match the declarations in server/src/interfaces.ts
-export const serverError = new NotificationType<string>('serverErrorNotification');
-export const serverDebug = new NotificationType<string>('serverDebugNotification');
-export const serverStartWork = new NotificationType0('serverStartWork');
-export const serverStopWork = new NotificationType0('serverStopWork');
-export const clientDataSend = new NotificationType<string>('clientDataSend');
-
-const RECENT_CHOICES_KEY = 'recentChoicesMooseLanguage';
+const RECENT_CHOICES_KEY = 'neml2_language_server_recent_choices';
 const MAX_RECENT_CHOICES = 5;
 
-let statusDisposable: Disposable | null;
+// The NEML2 language server picked by the user last time
+// undefined: the user hasn't made a choice yet, and so the selector will be shown
+// null: the user opted out of language features
+let server_path: string | null | undefined = undefined;
 
-// undefined means the user rejected autocomplete, null means the selector will be shown
-let lastExecutablePick: string | null | undefined = null;
+function try_start() {
+	if (!client) { return; }
 
-async function showRestartError() {
-    const restart = 'Restart server.';
-    const chosen = await window.showErrorMessage("MOOSE language server connection closed.", restart);
-    if (chosen === restart) {
-        pickServer()
-    }
+	client.start()
+		.then(() => { /* maybe show a running indicator */ })
+		.catch(() => {
+			window.showErrorMessage("Failed to start NEML2 language server.");
+			client = null;
+			server_path = null;
+		});
 }
 
-function tryStart() {
-    client.start()
-        .then(() => { /* maybe show a running indicator */ })
-        .catch(() => {
-            window.showErrorMessage("Failed to start MOOSE executable. You might need to rebuild it.");
-            client = null;
-            lastExecutablePick = null;
-        });
+function get_recent_choices(): string[] {
+	return ctx.globalState.get<string[]>(RECENT_CHOICES_KEY) || [];
 }
 
-function getRecentChoices(): string[] {
-    return my_context.globalState.get<string[]>(RECENT_CHOICES_KEY) || [];
+function update_recent_choices(choice: string) {
+	let choices = get_recent_choices();
+	choices = [choice, ...choices.filter(c => c !== choice)];
+	if (choices.length > MAX_RECENT_CHOICES) {
+		choices = choices.slice(0, MAX_RECENT_CHOICES);
+	}
+	ctx.globalState.update(RECENT_CHOICES_KEY, choices);
 }
 
-function updateRecentChoices(choice: string) {
-    let recentChoices = getRecentChoices();
-    recentChoices = [choice, ...recentChoices.filter(c => c !== choice)];
-    if (recentChoices.length > MAX_RECENT_CHOICES) {
-        recentChoices = recentChoices.slice(0, MAX_RECENT_CHOICES);
-    }
-    my_context.globalState.update(RECENT_CHOICES_KEY, recentChoices);
+async function pick_server(): Promise<string | null | undefined> {
+	if (!doc) { return undefined; }
+
+	// env var
+	const env_var = 'NEML2_LANGUAGE_SERVER';
+	if (env_var in process.env) {
+		return process.env[env_var];
+	}
+
+	// user opted out of autocomplete for now
+	if (server_path === null) {
+		window.showInformationMessage("NEML2 Language Server disabled.");
+		return null;
+	}
+
+	// prompt user to pick an executable
+	if (server_path === undefined) {
+		// find server_candidates (up the path)
+		let server_candidates = new Map<Date, QuickPickItem>();
+		let uri = doc.uri;
+
+		// we might have an "untitled" editor (an non existing file that was opend using the command line)
+		// let's just drop the `untitled:` scheme and hope for the best. In the worst case the user can still
+		// manually select an executable (or use one from the list of recent ones).
+		if (uri.scheme === 'untitled') {
+			uri = uri.with({ scheme: 'file' });
+		}
+
+		while (true) {
+			// keep searching in the parent dir
+			let newuri = Uri.joinPath(uri, "..");
+			if (newuri === uri) { break; }
+			uri = newuri;
+			// look for `langserv` in the current directory
+			for (const [name, type] of await workspace.fs.readDirectory(uri)) {
+				if (name === "langserv") {
+					let fileuri = Uri.joinPath(uri, name);
+					let p = fileuri.fsPath;
+					try {
+						// check if p is executable
+						fs.accessSync(p, fs.constants.X_OK);
+						// get modification time
+						server_candidates.set(fs.statSync(p).mtime, {
+							label: p,
+							detail: 'Last updated ' + fs.statSync(p).mtime.toLocaleString()
+						});
+					} catch (err) {
+						continue;
+					}
+				}
+			}
+		}
+
+		// no server candidate found
+		if (server_candidates.size === 0) {
+			window.showInformationMessage("No NEML2 language server found in the current directory or any of the parent directories.");
+			return null;
+		}
+
+		// sort by modification time
+		let server_candidates_sorted = new Map([...server_candidates.entries()].sort());
+
+		// build quick pick items
+		let items: QuickPickItem[] = [...server_candidates_sorted.values()];
+		items = items.concat([{
+			label: 'Other options...',
+			kind: QuickPickItemKind.Separator
+		},
+		{
+			label: "Open File...",
+			detail: 'Manually select the language server'
+		}]);
+		const recent = get_recent_choices();
+		if (recent.length > 0) {
+			items.push({
+				label: 'Recently used language servers',
+				kind: QuickPickItemKind.Separator
+			});
+			items = items.concat(recent.map(name => ({ label: name })));
+		}
+
+		// prompt the user to pick a server
+		const server_pick = await window.showQuickPick(items, { placeHolder: 'NEML2 language server' });
+
+		// no selection
+		if (!server_pick) { return undefined; }
+
+		// user wants to manually select a server
+		if (server_pick.label === 'Open File...') {
+			const selection = await window.showOpenDialog({
+				canSelectFiles: true,
+				canSelectMany: false,
+				filters: {}
+			});
+
+			if (selection && selection[0]) {
+				return selection[0].fsPath;
+			}
+			else {
+				return undefined;
+			}
+		}
+
+		// otherwise return the selected server
+		return server_pick.label;
+	}
+
+	// otherwise return the last selected server
+	return server_path;
 }
 
-async function pickServer() {
-    if (!currentDocument) {
-        return;
-    }
+async function restart_server() {
+	server_path = await pick_server();
+	if (!server_path) { return; }
+	update_recent_choices(server_path);
 
-    // env var set?
-    const env_var = 'MOOSE_LANGUAGE_SERVER'
-    if (env_var in process.env) {
-        lastExecutablePick = process.env[env_var];
-    }
-    else {
-        // user opted out of autocomplete for now
-        if (lastExecutablePick === undefined) {
-            let enable = "Enable";
-            window.showInformationMessage("MOOSE Language Server disabled.", enable)
-                .then(selection => {
-                    if (selection == enable) {
-                        lastExecutablePick = null;
-                        pickServer();
-                    }
-                })
-            return;
-        }
+	// build server options
+	const serverOptions: ServerOptions = {
+		command: server_path
+	};
 
-        // prompt user to pick an executable
-        if (lastExecutablePick === null) {
-            // find executables (up the path)
-            let executables = [];
-            let uri = currentDocument.uri;
+	// options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// register the server for NEML2 input files
+		documentSelector: [{ scheme: 'file', language: 'neml2' }, { scheme: 'untitled', language: 'neml2' }]
+	};
 
-            // we might have an "untitled" editor (an non existing file that was opend using the command line)
-            // let's just drop the `untitled:` scheme and hope for the best. In the worst case the user can still
-            // manually select an executable (or use one from the list of recent ones).
-            if (uri.scheme == 'untitled') {
-                uri = uri.with({ scheme: 'file' });
-            }
+	// create the language client and start the client.
+	client = new LanguageClient(
+		'language-server-neml2',
+		'NEML2 Language Server',
+		serverOptions,
+		clientOptions
+	);
 
-            const pattern = /(-opt|-dev|-oprof|-devel)$/;
-            while (true) {
-                // parent dir
-                let newuri = Uri.joinPath(uri, "..");
-                if (newuri == uri) break;
-                uri = newuri;
+	// handle notifications
+	client.onNotification("neml2/debug", (msg: string) => { console.log(msg); });
 
-                // list directory
-                for (const [name, type] of await workspace.fs.readDirectory(uri)) {
-                    if (type !== FileType.Directory && pattern.exec(name)) {
-                        let fileuri = Uri.joinPath(uri, name);
-
-                        let p = fileuri.fsPath;
-                        try {
-                            // check if p is executable
-                            fs.accessSync(p, fs.constants.X_OK);
-
-                            // get modification time
-                            let stat = fs.statSync(p);
-                            executables.push(
-                                {
-                                    mtime: stat.mtime,
-                                    item: {
-                                        label: p,
-                                        detail: 'Last updated ' + formatDistance(stat.mtime, new Date(), { addSuffix: true })
-                                    }
-                                });
-                        } catch (err) {
-                            continue;
-                        }
-                    }
-                }
-            }
-
-            // sort by modification time
-            executables.sort((a, b) => b.mtime - a.mtime);
-
-            // items
-            let items: QuickPickItem[] = executables.map(e => e.item);
-
-            // add file selector
-            items = items.concat([{
-                label: 'Other options...',
-                kind: QuickPickItemKind.Separator
-            },
-            { label: "Open File...", detail: 'Manually select an executable' }]);
-
-            // add recent choices
-            const recent = getRecentChoices();
-            if (recent.length > 0) {
-                items.push({
-                    label: 'Recently used executables',
-                    kind: QuickPickItemKind.Separator
-                });
-                items = items.concat(recent.map(name => ({ label: name })));
-            }
-
-            // build quick pick
-            const result = await window.showQuickPick(items, {
-                placeHolder: 'MOOSE Executable'
-            });
-
-            // no selection
-            if (!result) {
-                lastExecutablePick = undefined;
-                return;
-            }
-
-            // otherwise start a server
-            if (result.label == 'Open File...') {
-                const fileUri = await window.showOpenDialog({
-                    canSelectFiles: true,
-                    canSelectMany: false,
-                    filters: {}
-                });
-
-                if (fileUri && fileUri[0]) {
-                    lastExecutablePick = fileUri[0].fsPath;
-                } else {
-                    lastExecutablePick = undefined;
-                    return;
-                }
-            } else {
-                lastExecutablePick = result.label;
-            }
-        }
-
-        updateRecentChoices(lastExecutablePick);
-    }
-
-    let executable = lastExecutablePick;
-
-    // The debug options for the server
-    // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-    const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
-
-    // build server options
-    let ls_args = ['--language-server'];
-    const config_test_objects = workspace.getConfiguration('languageServerMoose').get<bool>("allowTestObjects");
-    if (config_test_objects) {
-        ls_args.push('--allow-test-objects')
-    }
-    const serverOptions: ServerOptions = {
-        command: executable,
-        args: ls_args
-    };
-
-    // watch item and restart server upon changes
-    try {
-        let lastMtime = fs.statSync(executable).mtime;
-
-        fs.watch(executable, { persistent: false }, (eventType, name) => {
-            if (eventType == 'change' && client !== null) {
-                // change gets fired for all kinds of things. here we check the modification time of the file.
-                let mtime = fs.statSync(executable).mtime;
-                if (mtime <= lastMtime)
-                    return;
-                lastMtime = mtime;
-
-                window.showInformationMessage("MOOSE executable was updated, restarting language server.");
-                client.stop().then(() => { tryStart(); });
-            }
-            if (eventType == 'rename') {
-                window.showInformationMessage("MOOSE language server executable was renamed or deleted.");
-            }
-        });
-    } catch (err) {
-    }
-
-    // Options to control the language client
-    const clientOptions: LanguageClientOptions = {
-        // Register the server for MOOSE input files
-        documentSelector: [{ scheme: 'file', language: 'moose' }, { scheme: 'untitled', language: 'moose' }]
-    };
-
-    // Create the language client and start the client.
-    client = new LanguageClient(
-        'languageServerMoose',
-        'MOOSE Language Server',
-        serverOptions,
-        clientOptions
-    );
-
-    // handle notifications
-    client.onNotification(serverError, (msg: string) => {
-        window.showErrorMessage(msg);
-    });
-    client.onNotification(serverDebug, (msg: string) => {
-        console.log(msg);
-    });
-
-    client.onNotification(serverStartWork, () => {
-        if (statusDisposable) {
-            statusDisposable.dispose();
-        }
-    });
-    client.onNotification(serverStopWork, () => {
-        if (statusDisposable) {
-            statusDisposable.dispose();
-        }
-        statusDisposable = null;
-    });
-
-    // Start the client. This will also launch the server
-    tryStart();
+	// start the client (This will also launch the server.)
+	try_start();
 }
 
 export async function activate(context: ExtensionContext) {
-    let editor = window.activeTextEditor;
-    if (editor)
-        currentDocument = editor.document;
-    my_context = context;
-    pickServer();
+	let editor = window.activeTextEditor;
+	if (editor) {
+		doc = editor.document;
+	}
+	ctx = context;
+	restart_server();
 
-    // If no server is running yet and we switch to a new MOOSE input, we offer the choice again
-    window.onDidChangeActiveTextEditor(editor => {
-        if (!editor) {
-            editor = window.activeTextEditor;
-            if (!editor) {
-                return;
-            }
-        }
-        if (currentDocument === editor.document) {
-            return;
-        }
+	// If no server is running yet and we switch to a new NEML2 input, we offer the choice again
+	window.onDidChangeActiveTextEditor(editor => {
+		if (!editor) {
+			editor = window.activeTextEditor;
+			if (!editor) { return; }
+		}
+		if (doc === editor.document) { return; }
 
-        if (editor.document.languageId === 'moose') {
-            currentDocument = editor.document;
-            if (!client) {
-                pickServer();
-            }
-        }
-    });
+		if (editor.document.languageId === 'neml2') {
+			doc = editor.document;
+			if (!client) { restart_server(); }
+		}
+	});
 
-    // add command
-    context.subscriptions.push(commands.registerCommand('mooseLanguageSupport.startServer', async () => {
-        lastExecutablePick = null;
-        if (client)
-            client.stop().then(() => { client = null; pickServer(); });
-        else
-            pickServer();
-    }));
+	// add command (this should match the declaration in package.json)
+	context.subscriptions.push(commands.registerCommand('neml2-language-support.start-server', async () => {
+		server_path = undefined;
+		if (client) {
+			client.stop().then(() => { client = null; restart_server(); });
+		}
+		else {
+			restart_server();
+		}
+	}));
 
-    // update language specific configuration
-    const config = workspace.getConfiguration("", { languageId: "moose" });
-    config.update("outline.showProperties", false, false, true);
-    config.update("outline.showStrings", false, false, true);
-    config.update("gitlens.codeLens.scopes", ['document'], false, true);
+	// update language specific configuration
+	const config = workspace.getConfiguration("", { languageId: "neml2" });
+	config.update("outline.showProperties", false, false, true);
+	config.update("outline.showStrings", false, false, true);
 }
 
 export function deactivate(): Thenable<void> | undefined {
-    if (!client) {
-        return undefined;
-    }
-    return client.stop();
+	if (!client) { return undefined; }
+	return client.stop();
 }
